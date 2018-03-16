efe5ec921e6f798b2e8a5ae27461cafe4a6d7e61
Shahina <shahina@qburst.com>
2018-03-16 16:56:36 +0530

    Webpack configured and created the initial folder structure for the application

---
+++ src/scenes/YoutubeSearchApp/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class YoutubeSearchApp extends React.Component {
+    render() {
+        return (
+            <div>Shahina Anvar</div>
+        )
+    }
+}
+
+export default YoutubeSearchApp;
